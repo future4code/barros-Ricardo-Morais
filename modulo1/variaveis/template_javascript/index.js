706853b09dcc400d4a84c1e3cdee15ee18163395
Ricardo Barros de Morais <ricardo_teleco@hotmail.com>
2022-04-12 22:48:06 -0300

    Exercício JS

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,46 @@
+// Exercicío 
+
+// 1. Será impresso (10, 5)
+
+// 2. Será impresso (10, 10, 10)*/
+
+// 3. let horasTrabalhadasDia = prompt("Quantas horas você trabalha por dia?")
+  // let salarioDia = prompt("Quanto você recebe por dia?")
+  // alert(`Voce recebe ${t/p} por hora`)
+
+/*1. Exercício de escrita de código.
+ 
+d) Se eu coloco aspas ele mostra "sting", mas se eu não coloco nada
+ele não consegue definir o tipo de variável, e dá um erro.*/
+
+/*let nome = prompt("Qual o seu nome? ")
+let idade = prompt("Qual a sua idade? ")
+console.log(typeof nome)
+console.log(typeof idade)
+f) Pra mim apareceu duas strings, achei estranho porque idade é um número.*/
+
+//g)
+let nome = prompt("Qual o seu nome? ")
+let idade = prompt("Qual a sua idade? ")
+console.log("Olá", nome, "você tem", idade, "anos.")
+
+//2. Exercício
+let resp1 = prompt("Hoje é domingo? ")
+let resp2 = prompt("Você é estudante da Labenu? ")
+let resp3 = prompt("Você está com fome? ")
+diaDasemana = resp1
+estudanteLabenu = resp2
+fome = resp3
+console.log("Hoje é domingo? ", diaDasemana)
+console.log("Você é estudante da Labenu? ", estudanteLabenu)
+console.log("Você está com fome? ", fome)
+ 
+//3. Exercício
+let a = 10
+let b = 25
+c = a
+a = b
+b = c
+console.log("O novo valor de a é", a) 
+console.log("O novo valor de b é", b) 
+

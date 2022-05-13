aa0b476e76ab9aa33d98b3bf49540ba601941bc6
Ricardo Barros de Morais <ricardo_teleco@hotmail.com>
2022-05-12 22:35:23 -0300

    Até o exercício 11 ok

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -7,14 +7,11 @@
 function retornaTamanhoArray(array) {
    return array.length
 }
-array = [8, 9, 3]
-
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
   return array.reverse()
 }
-array = [8, 9, 3, 5]
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
@@ -43,36 +40,87 @@ function retornaNumerosParesElevadosADois(array) {
             novoArray.push(numerosPares1)
         }
     }
-        let numerosDobro = novoArray.map(item) {
-            return numerosDobro * numerosDobro={}
-        }
+        let numerosDobro = novoArray.map((numero) => {
+            return (Math.pow(numero, 2))
+        })
+    return numerosDobro
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+    let maiorNumero = array[0]
+ 
+    for (let i = 1; i < array.length; i++) {
+       if (array[i] > maiorNumero) {
+          maiorNumero = array[i]
+       }
+    }
+    return maiorNumero
 }
+
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-
+    let maiorNumero = 0
+    let menorNumero = 0
+    let divisao
+    if(num1 > num2){
+        maiorNumero = num1
+    }else {
+        maiorNumero = num2
+    }
+    if(num1 < num2){
+        menorNumero = num1
+    }else {
+        menorNumero = num2
+    }
+    if(maiorNumero % menorNumero === 0){
+        divisao = true
+    }else {
+        divisao = false
+    }
+    let objeto = {
+        maiorNumero: maiorNumero,
+        maiorDivisivelPorMenor: divisao,
+        diferenca: maiorNumero - menorNumero,
+    }
+    return objeto
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+   const numero = []
+   for (let i = 0; numero.length < n; i++) {
+       if(i % 2 === 0) {
+           numero.push(i)
+       }
+   }
+   return numero
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+    if(ladoA !== ladoB && ladoA !== ladoC && ladoB !== ladoC) {
+        return "Escaleno"
+    }else if (ladoA === ladoB && ladoA === ladoC && ladoB === ladoC){
+        return "Equilátero"
+    }else if(ladoA === ladoB || ladoB === ladoC || ladoA === ladoC) {
+        return "Isósceles"
+    }
+    
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+    let array2 = []
+    const crescente = array.sort((item, index) =>{
+        return item - index
+    })
+    let segundoMaiorNumero = crescente[crescente.length -2]
+    let segundoMenorNumero = crescente[1]
+array2.push(segundoMaiorNumero, segundoMenorNumero)
+    return array2
 }
-
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
    

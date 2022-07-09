380b12bfcc3498106cdf7edbee3fbcd8fd6ea2c3
Ricardo Barros de Morais <ricardo_teleco@hotmail.com>
2022-07-08 21:06:02 -0300

    Iniciando o projeto-whatslab

---
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Final de semana eu termino!</h1>
+    </div>
+  );
+}
+
+export default App;

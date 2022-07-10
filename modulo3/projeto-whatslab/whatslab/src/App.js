f24d6aef939d26802caef4905a2fb1a4a733d0d7
Ricardo Barros de Morais <ricardo_teleco@hotmail.com>
2022-07-09 21:23:16 -0300

    Projeto-Whatslab Primeira Estilização

--- modulo3/projeto-whatslab/whatslab/src/App.js
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -1,10 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { Header, Footer, Main, Left, Right } from './components/style';
+import Logolab from './img/logo-lab.png'
+import Formulario from './components/Formulario';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Final de semana eu termino!</h1>
+      <img src={Logolab} />
+      <Header>
+        Labzap
+      </Header>
+
+      <Left>
+          Left
+      </Left> 
+
+      <Main>
+        Main
+
+        
+      </Main>
+
+      <Right>
+        Right
+      </Right>
+
+      <Footer>
+       <h4> Copyright © 2022 Ricardo All Rights Reserved || São Paulo-SP</h4>
+      </Footer>
+
     </div>
   );
 }
---
+++ modulo3/projeto-whatslab/whatslab/src/components/style.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const Header = styled.h1`
+    color: orange;
+
+` 
+export const Left = styled.h1`
+    display: inline-block;
+    height: 600px;
+    width: 200px;
+    background-color: orange;
+`
+export const Right = styled.h1`
+    display: inline-block;
+    
+    height: 600px;
+    width: 200px;
+    background-color: orange;
+   
+`
+export const Main = styled.h1`
+   display: inline-block;
+   justify-content: space-between;
+   margin-left: 5px;
+   height: 600px;
+   width: 940px;
+   background-color: white;
+`
+export const Footer = styled.h1`
+    text-align: center;
+    height: 70px;
+    background-color: gray;
+    
+`

dfee130b34717a0df2daad033f2b65448289d3df
Ricardo Barros de Morais <ricardo_teleco@hotmail.com>
2022-10-07 21:52:02 -0300

    Adicionando exercícios incompleto

--- modulo5/revisao-javascript-backend/backend/exercicio3/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio3/index.js
@@ -1,1 +1,25 @@
-console.log("exercicio 3");
+const contas = [
+	{
+		email: "astrodev@labenu.com",
+		password: "abc123"
+	},
+	{
+		email: "bananinha@gmail.com",
+		password: "bananinha"
+	}
+]
+
+
+
+const loginOk = (contas) => {
+    for (let i = 0; i < contas.email.length; i++) {
+        if (contas.email.length([i]) === "@") {
+            console.log("Sucesso");
+            break
+        }else {
+            console.log("erro");
+        }
+    }
+}
+
+loginOk()

c8fb72ca8034e24c95c248e5507ffdfc3100f1bf
Ricardo Barros de Morais <ricardo_teleco@hotmail.com>
2022-10-11 22:28:25 -0300

    Adicionando Exercicio node-package-json

---
+++ modulo5/node-package-json/exercicio1/index.js
@@ -0,0 +1,18 @@
+/// 1-
+
+/// A- Usando o método process.argv
+
+/// B-
+
+/// const nome = process.argv[2]
+/// const idade = process.argv[3]
+
+/// console.log(`"Olá, ${nome}! Você tem ${idade} anos."`);
+
+/// C-
+
+/// const nome = process.argv[2]
+/// const idade = Number(process.argv[3])
+/// const idadeFutura = idade + 7
+
+/// console.log(`"Olá, ${nome}! Você tem ${idadeFutura} anos."`);
---
+++ modulo5/node-package-json/exercicio2/index.js
@@ -0,0 +1,6 @@
+/// 2-
+/// const n1 = Number(process.argv[2])
+/// const n2 = Number(process.argv[3])
+/// const mult = n1 * n2
+
+/// onsole.log(mult);

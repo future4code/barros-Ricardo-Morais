768138396946082846ebb0653bcb54dcede610f0
Ricardo Barros de Morais <ricardo_teleco@hotmail.com>
2022-11-28 21:55:55 -0300

    Atualizando exercício

--- modulo5/servicos-backend/src/index.ts
+++ modulo5/servicos-backend/src/index.ts
@@ -1,4 +1,8 @@
 import app from "./app"
 import createUser from './endpoints/createUser'
+import testAddress from "./endpoints/test"
+
 
 app.post('/users/signup', createUser)
+
+app.post('/test', testAddress)
